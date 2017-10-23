1f3ded29345a40f3855d6ca948fa5b89a0b873f9
Nithya Natarajan <nithyanatarajn@gmail.com>
2017-10-23 23:18:17 +0530

    Uniquely identifying article by id

--- src/main/java/Article.java
+++ src/main/java/Article.java
@@ -1,11 +1,12 @@
 import java.util.Objects;
 
 public class Article {
+    private final String id;
     private final String name;
     private final double price;
 
-    public Article(String name, double price) {
-
+    public Article(String id, String name, double price) {
+        this.id = id;
         this.name = name;
         this.price = price;
     }
@@ -23,11 +24,13 @@ public class Article {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Article article = (Article) o;
-        return Double.compare(article.price, price) == 0 && Objects.equals(name, article.name);
+        return Objects.equals(id, article.id)
+                && Objects.equals(name, article.name)
+                && Double.compare(article.price, price) == 0;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, price);
+        return Objects.hash(id, name, price);
     }
 }
--- src/test/java/ArticleTest.java
+++ src/test/java/ArticleTest.java
@@ -1,22 +1,41 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ArticleTest {
 
     @Test
     public void shouldCreateArticle() {
-        Article article = new Article("Pencil", 3.5);
+        Article article = getPencilWithId("Art0001");
 
         assertEquals("Pencil", article.getName());
         assertEquals(3.5, article.getUnitPrice(), 0);
     }
 
     @Test
-    public void twoSameArticlesShouldBeEquivalent() {
-        Article pencil1 = new Article("Pencil", 3.5);
-        Article pencil2 = new Article("Pencil", 3.5);
+    public void shouldReturnTrueWhenComparingTwoSameArticles() {
+        Article pencil1 = getPencilWithId("Art0001");
+        Article pencil2 = getPencilWithId("Art0001");
 
         assertEquals(pencil1, pencil2);
+    }
+
+    @Test
+    public void shouldReturnFalseWhenComparingTwoDifferentArticles() {
+        Article pencil1 = getPencilWithId("Art0001");
+        Article pencil2 = getPencilWithId("Art0002");
+
+        assertNotEquals(pencil1, pencil2);
+    }
+
+    @Test
+    public void shouldComputeTaxWhenComputingPrice() {
+        Article pencil = getPencilWithId("Art0001");
+
+    }
+
+    private Article getPencilWithId(String id) {
+        return new Article(id, "Pencil", 3.5);
     }
 }
